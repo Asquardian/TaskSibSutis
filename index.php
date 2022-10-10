9fa1a5e660b0c6cbb4366cf8192665a7d6c3a813
dainl <alexromanov5262@gmail.com>
2022-10-10 20:06:06 +0300

    First look

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <html>
 
 <body>
-    Hello
+    <form action="PLACEHOLDER" method="POST">
+        <table>
+            <tr>
+                <td>
+                    <a>ФИО:</a>
+                </td>
+                <td>
+                    <input name="NameOfStudent" type="text"></input>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a>Группа:</a>
+                </td>
+                <td>
+                    <input name="Group" type="text"></input>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a>Количество:</a>
+                </td>
+                <td>
+                    <input name="Amount" type="number"></input>
+                </td>
+            </tr>
+        </table>
+        <button>Отправить заявку</button>
+    </form>
 </body>
+
 </html>
